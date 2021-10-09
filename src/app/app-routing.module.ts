9a2b57e0b378022ebd5a38a4cedbd2c929229127
fallou <fallou-du-13@hotmail.fr>
2021-10-09 23:43:41 +0200

    presentation page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PresentationComponent } from './components/presentation/presentation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PresentationComponent},
+  { path: 'presentation', component: PresentationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule { 
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PresentationComponent
   ],
   imports: [
     BrowserModule,
